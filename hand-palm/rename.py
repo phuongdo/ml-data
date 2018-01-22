d35b221c459be0e2f01167ddf3f97f5ff284af86
phuongdv <dovietphuong.ptit@gmail.com>
2018-01-22 10:40:48 +0700

    add test case

---
+++ hand-palm/rename.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+directoryName = "test-sen"
+filePath = os.path.abspath(directoryName)
+filePathWithSlash = filePath + "/"
+for counter, filename in enumerate(os.listdir(directoryName)):
+  filenameWithPath = os.path.join(filePathWithSlash, filename)
+  os.rename(filenameWithPath, filenameWithPath.replace(filename,"IMG_" + str(counter+1).zfill(3) + ".jpg" ))
+  
