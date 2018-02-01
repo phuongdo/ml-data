11563d19ae8d8aa8f3ddea61cceef976832e8333
phuongdv <dovietphuong.ptit@gmail.com>
2018-02-01 15:24:33 +0700

    add

--- hand-palm/python/rename.py
+++ hand-palm/python/rename.py
@@ -2,10 +2,10 @@ import os
 import os
 import sys
 
-directoryName = "test-neg"
+directoryName = "test-pos"
 filePath = os.path.abspath(directoryName)
 filePathWithSlash = filePath + "/"
 for counter, filename in enumerate(os.listdir(directoryName)):
   filenameWithPath = os.path.join(filePathWithSlash, filename)
-  os.rename(filenameWithPath, filenameWithPath.replace(filename,"IMG_" + str(counter+1).zfill(5) + ".jpg" ))
+  os.rename(filenameWithPath, filenameWithPath.replace(filename,"IMG_" + str(counter+1).zfill(3) + ".jpg" ))
   
